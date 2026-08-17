c9fcb05bf895990075f3a2e357ec06d3fd733689
Andrei Drozd <a.drozd@evernode.tech>
2026-08-17 13:38:10 +0300

    models added

---
+++ DrozdHW_AQA/DTO/CreateUserRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TestAQA1
+{
+    public class CreateUserRequestDTO
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("job")]
+        public string Job { get; set; }
+    }
+}
