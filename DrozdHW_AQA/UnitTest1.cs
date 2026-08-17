d58b95c56ed07ef6768d456841b727138a2ff238
Andrei Drozd <a.drozd@evernode.tech>
2026-08-17 13:31:20 +0300

    base

--- DrozdHW_AQA/UnitTest1.cs
+++ DrozdHW_AQA/UnitTest1.cs
@@ -1,16 +1,42 @@
-﻿namespace DrozdHW_AQA
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace DrozdHW_AQA
 {
     public class Tests
     {
-        [SetUp]
+        private static HttpClient client;
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            client = new HttpClient
+            {
+                BaseAddress = new Uri("https://reqres.in/api/")
+            };
+            client.DefaultRequestHeaders.Add("x-api-key", "free_user_3I2DtUwiSzKQ4zy8x37TOPvgHRa");
         }
 
         [Test]
-        public void Test1()
+        public async Task Test1()
         {
-            Assert.Pass();
+            using HttpResponseMessage response = await client.GetAsync("users/2");
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Test]
+        public async Task Test2()
+        {
+            using HttpResponseMessage response = await client.GetAsync("users/2");
+            string jsonGet = await response.Content.ReadAsStringAsync();
+            UserResponseDTO userResponse = JsonSerializer.Deserialize<UserResponseDTO>(jsonGet);
+            UserDataDTO user = userResponse.Data;
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            client.Dispose();
         }
     }
 }
