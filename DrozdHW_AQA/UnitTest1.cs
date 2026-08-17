49f30389ae97bc85b5c387862c4fbc5aa1c7b80d
Andrei Drozd <a.drozd@evernode.tech>
2026-08-17 12:17:56 +0300

    Add project files.

---
+++ DrozdHW_AQA/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace DrozdHW_AQA
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
