b0cc68cd674852e5ddc3362544a317b3b2d66aa5
Andrei Drozd <a.drozd@evernode.tech>
2026-08-18 20:23:17 +0300

    Refit start + interface

---
+++ DrozdHW_AQA/Interfaces/Interface1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace DrozdHW_AQA.Interfaces
+{
+    [Headers(("x-apikey: free_user_3I2DtUwiSzKQ4zy8x37TOPvgHRa"))]
+    public interface IUserApi
+    {
+        [Get("users/2")]
+        Task<UserResponseDTO> GetUserAsync(int id);
+    }
+
+}
---
+++ DrozdHW_AQA/Tests/RefitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+using Microsoft.Extensions.DependencyInjection;
+using DrozdHW_AQA.Interfaces;
+
+namespace DrozdHW_AQA.Tests.Tests
+{
+    public class RefitTests
+    {
+        private IUserApi api;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            var services = new ServiceCollection();
+            services.AddRefitClient<IUserApi>()
+                .ConfigureHttpClient(c =>
+                {
+                    c.BaseAddress = new Uri("https://reqres.in/api");
+                });
+            var provider = services.BuildServiceProvider();
+            api = provider.GetRequiredService<IUserApi>();
+        }
+
+    }
+}
