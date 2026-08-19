396da4909d4276525f98c0202b70391074d01945
Andrei Drozd <a.drozd@evernode.tech>
2026-08-19 09:31:47 +0300

    interface added

--- DrozdHW_AQA/Interface/IUserApi.cs
+++ DrozdHW_AQA/Interface/IUserApi.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using Refit;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using TestAQA1;
+using Tests1.DTO;
 
-namespace DrozdHW_AQA.Interface
+namespace Tests1.Interfaces
 {
-    internal interface IUserApi
+    [Headers("x-api-key: free_user_3I2DtUwiSzKQ4zy8x37TOPvgHRa")]
+    public interface IUserApi
     {
+        [Get("/users/{id}")]
+        Task<UserResponseDTO> GetUserAsync(int id);
+
+        [Post("/users")]
+        Task<CreateUserResponseDTO> CreateUserAsync([Body] CreateUserRequestDTO request);
+
+        [Delete("/users/{id}")]
+        Task<ApiResponse<string>> DeleteUserAsync(int id);
     }
 }
--- DrozdHW_AQA/Tests/RefitTests.cs
+++ DrozdHW_AQA/Tests/RefitTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Refit
+using Refit;
 
 namespace DrozdHW_AQA.Tests
 {
