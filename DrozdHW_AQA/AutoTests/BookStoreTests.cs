6a54a39095a27a33a76bb87bedc470aaa395fa46
Andrei Drozd <a.drozd@evernode.tech>
2026-09-07 00:01:25 +0300

    Negative bookstore test (invalid isbn)

--- DrozdHW_AQA/AutoTests/BookStoreTests.cs
+++ DrozdHW_AQA/AutoTests/BookStoreTests.cs
@@ -123,8 +123,25 @@ namespace Tests1.Tests
                 new List<CollectionOfIsbnsDTO> { new CollectionOfIsbnsDTO(rndIsbn) }
             );
 
-            Func<Task> act = async () => await api.AddBookToUserAsync(request, token: null); 
+            Func<Task> act = async () => await api.AddBookToUserAsync(request, token: null);
             act.Should().ThrowAsync<ApiException>(); //.Where(p => p.StatusCode == System.Net.HttpStatusCode.BadRequest) - по статус кодам почему-то не отрабатывает
+        }
+
+        [Test]
+        public async Task AddBookToUserWithInvalidIsbnAsync()
+        {
+            var token = await GetTokenAsync();
+
+            var userId = await GetUsersIdAsync();
+
+            var request = new AddCollectionOfBooksToUserDTO
+            (
+                userId,
+                new List<CollectionOfIsbnsDTO> { new CollectionOfIsbnsDTO("INVALID_ISBN") }
+            );
+
+            Func<Task> act = async () => await api.AddBookToUserAsync(request, token);
+            await act.Should().ThrowAsync<ApiException>();
         }
 
 
